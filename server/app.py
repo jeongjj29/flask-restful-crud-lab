de546e634271ac4bf7041f3403fb096c877322f2
Jae Jeong <jeongjj29@gmail.com>
2024-09-18 21:34:40 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -27,9 +27,9 @@ class Plants(Resource):
         data = request.get_json()
 
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
         )
 
         db.session.add(new_plant)
@@ -38,7 +38,7 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
 class PlantByID(Resource):
@@ -47,9 +47,27 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in data:
+            setattr(plant, attr, data[attr])
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
 
 
-if __name__ == '__main__':
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
